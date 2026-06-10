88f8ae68327e24f49f759b651aa3ec2e3252a154
disha23bctf71 <dpswain15@gmail.com>
2026-06-10 12:33:52 +0530

    Delete q1.kt

--- q1.kt
+++
@@ -1,7 +0,0 @@
-// 1. Write a Kotlin program to print numbers from 1 to 10 using a loop.
-fun main() {
-    // Using a simple for loop with a range from 1 to 10
-    for (i in 1..10) {
-        println(i)
-    }
-}
